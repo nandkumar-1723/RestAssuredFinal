5dc3691ed5fecdfee60d172202ced69e40b8edaf
nandkumar-1723 <127601507+nandkumar-1723@users.noreply.github.com>
2023-03-31 15:17:16 +0530

    n

--- src/test/java/ReadExcell/readexcell.java
+++ src/test/java/ReadExcell/readexcell.java
@@ -1,8 +1,5 @@
 package ReadExcell;
 
-import org.apache.poi.ss.usermodel.*;
-import org.apache.poi.xssf.usermodel.*;
-
 import java.io.*;
 
 /**
@@ -11,17 +8,7 @@ import java.io.*;
 public class readexcell {
     public static void main(String[] args) throws IOException {
 
-        File file = new File("src/test/java/com/jira/ReadExcell/TestCasesNew.xls");   //creating a new file instance
-        FileInputStream fis = new FileInputStream(file);   //obtaining bytes from the file
-
-        XSSFWorkbook wb = new XSSFWorkbook(fis);
-        XSSFSheet sheet = wb.getSheetAt(0);
-        String row=sheet.getRow(1).getCell(2).getStringCellValue(); //returns the logical row
-        System.out.println(row);
-
-            System.out.println(sheet.getLastRowNum());
-
-        System.out.println(sheet.getRow(1).getLastCellNum());
+        System.out.println("Nandkumar Babar");
 
 
     }
